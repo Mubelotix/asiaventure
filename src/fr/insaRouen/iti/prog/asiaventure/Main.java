354c6c9991483bc347e7512030b31be87a297156
Mubelotix <mubelotix@gmail.com>
2024-01-29 16:38:26 +0100

    Add test

--- src/fr/insaRouen/iti/prog/asiaventure/Main.java
+++ src/fr/insaRouen/iti/prog/asiaventure/Main.java
@@ -2,6 +2,7 @@ package fr.insaRouen.iti.prog.asiaventure;
 
 import fr.insaRouen.iti.prog.asiaventure.elements.PiedDeBiche;
 import fr.insaRouen.iti.prog.asiaventure.elements.ElementStructurel;
+import fr.insaRouen.iti.prog.asiaventure.elements.Entite;
 
 public class Main {
     public static void main(String[] args) {
@@ -11,6 +12,9 @@ public class Main {
         PiedDeBiche pdb = new PiedDeBiche("pdb", monde);
         System.out.println(monde);
         assert pdb.estDeplacable();
+
+        Entite pdb2 = monde.getEntite("pdb");
+        assert pdb2 == pdb;
 
         ElementStructurel es = new ElementStructurel("es", monde);
         System.out.println(monde);
