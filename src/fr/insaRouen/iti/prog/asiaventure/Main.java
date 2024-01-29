90049a3ee449321f080a9e5d938c229e5930701b
Mubelotix <mubelotix@gmail.com>
2024-01-29 16:06:00 +0100

    Add main

---
+++ src/fr/insaRouen/iti/prog/asiaventure/Main.java
@@ -0,0 +1,8 @@
+package fr.insaRouen.iti.prog.asiaventure;
+
+public class Main {
+    public static void main(String[] args) {
+        Monde monde = new Monde("monde");
+        System.out.println(monde);
+    }
+}
