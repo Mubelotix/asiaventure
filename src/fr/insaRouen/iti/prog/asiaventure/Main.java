2f5a887b4a0eeb6b7e0ce78165bbae09f3ddbdde
Mubelotix <mubelotix@gmail.com>
2024-01-29 16:34:26 +0100

    Improve main

--- src/fr/insaRouen/iti/prog/asiaventure/Main.java
+++ src/fr/insaRouen/iti/prog/asiaventure/Main.java
@@ -1,8 +1,18 @@
 package fr.insaRouen.iti.prog.asiaventure;
 
+import fr.insaRouen.iti.prog.asiaventure.elements.PiedDeBiche;
+import fr.insaRouen.iti.prog.asiaventure.elements.ElementStructurel;
+
 public class Main {
     public static void main(String[] args) {
         Monde monde = new Monde("monde");
+        System.out.println(monde);
+
+        PiedDeBiche pdb = new PiedDeBiche("pdb", monde);
+        System.out.println(monde);
+        assert pdb.estDeplacable();
+
+        ElementStructurel es = new ElementStructurel("es", monde);
         System.out.println(monde);
     }
 }
