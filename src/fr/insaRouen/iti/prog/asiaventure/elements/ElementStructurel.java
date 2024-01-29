646e9dc02140d575e70a0276ec4362708ae75d6b
Mubelotix <mubelotix@gmail.com>
2024-01-29 16:28:00 +0100

    Add ElementStructurel

---
+++ src/fr/insaRouen/iti/prog/asiaventure/elements/ElementStructurel.java
@@ -0,0 +1,9 @@
+package fr.insaRouen.iti.prog.asiaventure.elements;
+
+import fr.insaRouen.iti.prog.asiaventure.Monde;
+
+public class ElementStructurel extends Entite {
+    public ElementStructurel(String nom, Monde monde) {
+        super(nom, monde);
+    }
+}
