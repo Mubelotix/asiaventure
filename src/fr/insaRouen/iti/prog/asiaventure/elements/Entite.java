036a19142058fa73c7e7d9b28845c6f6f96b07d7
Mubelotix <mubelotix@gmail.com>
2024-01-29 15:48:53 +0100

    Add stuff

--- src/fr/insaRouen/iti/prog/asiaventure/elements/Entite.java
+++ src/fr/insaRouen/iti/prog/asiaventure/elements/Entite.java
@@ -1,5 +1,19 @@
 package fr.insaRouen.iti.prog.asiaventure.elements;
 
 public class Entite {
+    private Monde monde;
+    public String nom;
 
+    public Entite(String nom, Monde monde) {
+        this.monde = monde;
+        this.nom = nom;
+    }
+
+    private Monde getMonde() {
+        return this.monde;
+    }
+
+    private String getNom() {
+        return this.nom;
+    }
 }
