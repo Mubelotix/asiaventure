c5a9401ae2245365ea9d4abcc989908cbd4b611e
Mubelotix <mubelotix@gmail.com>
2024-02-12 15:54:40 +0100

    Add some doc

--- src/fr/insaRouen/iti/prog/asiaventure/elements/objets/Objet.java
+++ src/fr/insaRouen/iti/prog/asiaventure/elements/objets/Objet.java
@@ -3,11 +3,18 @@ package fr.insaRouen.iti.prog.asiaventure.elements.objets;
 import fr.insaRouen.iti.prog.asiaventure.Monde;
 import fr.insaRouen.iti.prog.asiaventure.elements.Entite;
 
+/**
+ * Classe abstraite représentant un objet dans le monde de jeu.
+ */
 public abstract class Objet extends Entite {
     public Objet(String nom, Monde monde) {
         super(nom, monde);
     }
 
+    /**
+     * Indique si l'objet est déplaçable.
+     * @return Vrai si l'objet est déplaçable, faux sinon.
+     */
     public abstract boolean estDeplacable();
 
     // -- Static utility methods --
