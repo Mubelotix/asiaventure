9fbf0e4ce74fa8260082d51c8008606c2ed73600
Mubelotix <mubelotix@gmail.com>
2024-02-12 15:29:37 +0100

    ObjetNonPossedeParLeVivantException

---
+++ src/fr/insaRouen/iti/prog/asiaventure/elements/vivants/ObjetNonPossedeParLeVivantException.java
@@ -0,0 +1,11 @@
+package fr.insaRouen.iti.prog.asiaventure.elements.vivants;
+
+public class ObjetNonPossedeParLeVivantException extends VivantException {
+    public ObjetNonPossedeParLeVivantException() {
+        super();
+    }
+
+    public ObjetNonPossedeParLeVivantException(String msg) {
+        super(msg);
+    }
+}
