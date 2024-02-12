8d86dbc43f87e10e9694e25b247d2f85835eb2cf
Mubelotix <mubelotix@gmail.com>
2024-02-12 17:11:33 +0100

    Fix a few issues

--- src/fr/insaRouen/iti/prog/asiaventure/Monde.java
+++ src/fr/insaRouen/iti/prog/asiaventure/Monde.java
@@ -24,9 +24,9 @@ public class Monde {
         return null;
     }
 
-    public boolean nomEntiteDejaUtilise(Entite entite){
+    private boolean nomEntiteDejaUtilise(Entite entite){
         for(int i = 0; i < this.entites.length; i++ ){
-            if (this.entites[i].getNom() == entite.getNom()) {
+            if (this.entites[i].getNom().equals(entite.getNom())) {
                 return true;
             }
         }
@@ -34,11 +34,11 @@ public class Monde {
     }
 
     public void ajouter(Entite entite) throws NomDEntiteDejaUtiliseDansLeMondeException, EntiteDejaDansUnAutreMondeException{
+        if(entite.getMonde().getNom().equals(this.getNom())) {
+            throw new EntiteDejaDansUnAutreMondeException(String.format("Pour le monde %s, impossible d'ajouter %s car cette entité est déja present dans %s", entite.getMonde().getNom(), entite.getNom(), this.getNom()));
+        }
         if (this.nomEntiteDejaUtilise(entite)) {
             throw new NomDEntiteDejaUtiliseDansLeMondeException(String.format("Monde : %s, Entite déjà utilisé : %s", this.getNom(), entite.getNom()));
-        }
-        if(entite.getMonde().getNom() != this.getNom()){
-            throw new EntiteDejaDansUnAutreMondeException(String.format("Pour le monde %s, impossible d'ajouter %s car cette entité est déja present dans %s", entite.getMonde().getNom(), entite.getNom(), this.getNom()));
         }
         Entite[] newEntites = new Entite[this.entites.length + 1];
         System.arraycopy(this.entites, 0, newEntites, 0, this.entites.length);
