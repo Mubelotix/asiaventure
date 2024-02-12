37b998acc306de4e4d70c786e58ae783337a13f8
Mubelotix <mubelotix@gmail.com>
2024-02-12 15:32:28 +0100

    Add PorteInexistanteDansLaPieceException

---
+++ src/fr/insaRouen/iti/prog/asiaventure/elements/structure/PorteInexistanteDansLaPieceException.java
@@ -0,0 +1,11 @@
+package fr.insaRouen.iti.prog.asiaventure.elements.structure;
+
+public class PorteInexistanteDansLaPieceException extends PieceException {
+    public PorteInexistanteDansLaPieceException() {
+        super();
+    }
+
+    public PorteInexistanteDansLaPieceException(String msg) {
+        super(msg);
+    }
+}
