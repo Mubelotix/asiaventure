dca22bcae9c0e1d714cde81f0b335c3c2dab4b36
mgevia <mgevia@insa-rouen.fr>
2024-02-12 15:19:55 +0100

    ajout de la classe NomDEntiteDejaUtiliseDansLeMondeException

---
+++ src/fr/insaRouen/iti/prog/asiaventure/NomDEntiteDejaUtiliseDansLeMondeException.java
@@ -0,0 +1,11 @@
+package fr.insaRouen.iti.prog.asiaventure;
+
+public class NomDEntiteDejaUtiliseDansLeMondeException extends ASIAventureException{
+    
+    public NomDEntiteDejaUtiliseDansLeMondeException(){
+    }
+
+    public NomDEntiteDejaUtiliseDansLeMondeException(String msg){
+        super(msg);
+    }
+}
