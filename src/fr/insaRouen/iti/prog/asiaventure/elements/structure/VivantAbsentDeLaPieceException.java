49272b8e824f41936807ed80c3d6c19ef3fda1a9
Mubelotix <mubelotix@gmail.com>
2024-02-12 15:33:09 +0100

    Add VivantAbsentDeLaPieceException

---
+++ src/fr/insaRouen/iti/prog/asiaventure/elements/structure/VivantAbsentDeLaPieceException.java
@@ -0,0 +1,11 @@
+package fr.insaRouen.iti.prog.asiaventure.elements.structure;
+
+public class VivantAbsentDeLaPieceException extends PieceException {
+    public VivantAbsentDeLaPieceException() {
+        super();
+    }
+
+    public VivantAbsentDeLaPieceException(String msg) {
+        super(msg);
+    }
+}
