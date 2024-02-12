c3e395406090c3dbbaa8fe3291196fec52453161
Mubelotix <mubelotix@gmail.com>
2024-02-12 15:19:41 +0100

    Create ElementStructurelException

---
+++ src/fr/insaRouen/iti/prog/asiaventure/elements/structure/ElementStructurelException.java
@@ -0,0 +1,13 @@
+package fr.insaRouen.iti.prog.asiaventure.elements.structure;
+
+import fr.insaRouen.iti.prog.asiaventure.ASIAventureException;
+
+public class ElementStructurelException extends ASIAventureException {
+    public ElementStructurelException() {
+        super();
+    }
+
+    public ElementStructurelException(String msg) {
+        super(msg);
+    }
+}
