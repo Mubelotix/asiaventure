b9c6bdabf7857f11f2376562da11a47300d610d9
Mubelotix <mubelotix@gmail.com>
2024-02-12 15:12:52 +0100

    Add ObjetNonDeplacableException

---
+++ src/fr/insaRouen/iti/prog/asiaventure/elements/objets/ObjetNonDeplacableException.java
@@ -0,0 +1,11 @@
+package fr.insaRouen.iti.prog.asiaventure.elements.objets;
+
+public class ObjetNonDeplacableException extends ObjetException {
+    public ObjetNonDeplacableException() {
+        super();
+    }
+
+    public ObjetNonDeplacableException(String msg) {
+        super(msg);
+    }
+}
