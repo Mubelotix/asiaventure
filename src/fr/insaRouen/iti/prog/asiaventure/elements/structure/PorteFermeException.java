0b67cbf2aa34ca8365b90b4eec896993cff091bf
Mubelotix <mubelotix@gmail.com>
2024-02-12 15:31:21 +0100

    Add PorteFermeException

---
+++ src/fr/insaRouen/iti/prog/asiaventure/elements/structure/PorteFermeException.java
@@ -0,0 +1,11 @@
+package fr.insaRouen.iti.prog.asiaventure.elements.structure;
+
+public class PorteFermeException extends ElementStructurelException {
+    public PorteFermeException() {
+        super();
+    }
+
+    public PorteFermeException(String msg) {
+        super(msg);
+    }
+}
