7877ceadbffdeec6d54e35fc0a4391f13943fbdd
Mubelotix <mubelotix@gmail.com>
2024-02-12 15:20:52 +0100

    Add ActivationException

---
+++ src/fr/insaRouen/iti/prog/asiaventure/elements/ActivationException.java
@@ -0,0 +1,13 @@
+package fr.insaRouen.iti.prog.asiaventure.elements;
+
+import fr.insaRouen.iti.prog.asiaventure.ASIAventureException;
+
+public class ActivationException extends ASIAventureException {
+    public ActivationException() {
+        super();
+    }
+
+    public ActivationException(String msg) {
+        super(msg);
+    }
+}
