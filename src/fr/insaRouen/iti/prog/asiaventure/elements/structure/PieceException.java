a58b13e04d504278bfd38f59067d8761cbd1fbda
Mubelotix <mubelotix@gmail.com>
2024-02-12 15:30:32 +0100

    Add PieceException

---
+++ src/fr/insaRouen/iti/prog/asiaventure/elements/structure/PieceException.java
@@ -0,0 +1,11 @@
+package fr.insaRouen.iti.prog.asiaventure.elements.structure;
+
+public class PieceException extends ElementStructurelException {
+    public PieceException() {
+        super();
+    }
+
+    public PieceException(String msg) {
+        super(msg);
+    }
+}
