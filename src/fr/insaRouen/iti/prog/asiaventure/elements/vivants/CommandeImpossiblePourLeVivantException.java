198005ec6a87a1833bed55e1adc920d887ceb935
Mubelotix <mubelotix@gmail.com>
2024-02-12 15:28:51 +0100

    Add CommandeImpossiblePourLeVivantException

---
+++ src/fr/insaRouen/iti/prog/asiaventure/elements/vivants/CommandeImpossiblePourLeVivantException.java
@@ -0,0 +1,11 @@
+package fr.insaRouen.iti.prog.asiaventure.elements.vivants;
+
+public class CommandeImpossiblePourLeVivantException extends VivantException {
+    public CommandeImpossiblePourLeVivantException() {
+        super();
+    }
+
+    public CommandeImpossiblePourLeVivantException(String msg) {
+        super(msg);
+    }
+}
