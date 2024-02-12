8dce1f88ec7b431350feb5302a594190b042e22b
Mubelotix <mubelotix@gmail.com>
2024-02-12 15:27:21 +0100

    Add VivantException

---
+++ src/fr/insaRouen/iti/prog/asiaventure/elements/vivants/VivantException.java
@@ -0,0 +1,13 @@
+package fr.insaRouen.iti.prog.asiaventure.elements.vivants;
+
+import fr.insaRouen.iti.prog.asiaventure.ASIAventureException;
+
+public class VivantException extends ASIAventureException {
+    public VivantException() {
+        super();
+    }
+
+    public VivantException(String msg) {
+        super(msg);
+    }
+}
