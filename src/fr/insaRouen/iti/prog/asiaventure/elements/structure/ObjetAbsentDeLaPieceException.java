9370e61b847746eeb15a021f597a95a38e108a2f
Mubelotix <mubelotix@gmail.com>
2024-02-12 15:38:15 +0100

    Add ObjetAbsentDeLaPieceException

---
+++ src/fr/insaRouen/iti/prog/asiaventure/elements/structure/ObjetAbsentDeLaPieceException.java
@@ -0,0 +1,11 @@
+package fr.insaRouen.iti.prog.asiaventure.elements.structure;
+
+public class ObjetAbsentDeLaPieceException extends PieceException {
+    public ObjetAbsentDeLaPieceException() {
+        super();
+    }
+
+    public ObjetAbsentDeLaPieceException(String msg) {
+        super(msg);
+    }
+}
