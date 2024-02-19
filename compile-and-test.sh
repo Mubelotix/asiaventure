38b25d2458bf18a75f3abf9d654c2bbe55af0173
mgevia <mgevia@insa-rouen.fr>
2024-02-19 15:37:34 +0100

    ajout des librairies

--- compile-and-test.sh
+++ compile-and-test.sh
@@ -2,7 +2,7 @@
 
 rm -rf classes classestests
 
-javac -classpath ./classes:./classestests:/usr/share/java/hamcrest-library.jar:/usr/share/java/junit4.jar -d ./classestest -sourcepath ./srctests/ ./srctests/fr/insaRouen/iti/prog/asiaventure/TestEntite.java
+javac -classpath ./classes:./classestests:/lib/hamcrest-library.jar:/lib/junit4.jar -d ./classestest -sourcepath ./srctests/ ./srctests/fr/insaRouen/iti/prog/asiaventure/TestEntite.java
 cd classestests
-java -ea org.junit.runner.JUnitCore:usr.share.java.hamcrest-library.jar:usr.share.java.junit4.jar: fr.insaRouen.iti.prog.asiaventure.TestEntite
+java -ea org.junit.runner.JUnitCore:lib.hamcrest-library.jar:lib.junit4.jar: fr.insaRouen.iti.prog.asiaventure.TestEntite
 cd ..
