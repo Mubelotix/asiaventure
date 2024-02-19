ca959775e64adba44b3b0f9c350111a4147075b8
Mubelotix <mubelotix@gmail.com>
2024-02-19 15:40:50 +0100

    Build jar

--- compile-and-run.sh
+++ compile-and-run.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
 rm -rf classes
+rm lib/asiaventure.jar
 
 javac -classpath ./classes -d ./classes/ -sourcepath ./src/ ./src/fr/insaRouen/iti/prog/asiaventure/Main.java 
+jar cvf lib/asiaventure.jar -C classes .
 cd classes
 java -ea fr.insaRouen.iti.prog.asiaventure.Main
 cd ..
