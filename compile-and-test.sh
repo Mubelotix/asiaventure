0f58928b7da06fa296f4112e0842c790bde3968e
mgevia <mgevia@insa-rouen.fr>
2024-02-19 15:44:57 +0100

    update hamcrest

--- compile-and-test.sh
+++ compile-and-test.sh
@@ -4,5 +4,5 @@ rm -rf classes classestests
 
 javac -classpath ./classes:./classestests:/lib/hamcrest-library.jar:/lib/junit4.jar -d ./classestest -sourcepath ./srctests/ ./srctests/fr/insaRouen/iti/prog/asiaventure/TestEntite.java
 cd classestests
-java -ea org.junit.runner.JUnitCore:lib.hamcrest-library.jar:lib.junit4.jar: fr.insaRouen.iti.prog.asiaventure.TestEntite
+java -ea org.junit.runner.JUnitCore:lib.hamcrest-library.jar:lib.junit4.jar fr.insaRouen.iti.prog.asiaventure.TestEntite
 cd ..
