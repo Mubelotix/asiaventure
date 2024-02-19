f8e255a4573f738d665357e2407e556ee77f1b36
Mubelotix <mubelotix@gmail.com>
2024-02-19 16:53:58 +0100

    Add Monde tests

--- srctests/fr/insaRouen/iti/prog/asiaventure/TestMonde.java
+++ srctests/fr/insaRouen/iti/prog/asiaventure/TestMonde.java
@@ -7,9 +7,16 @@ import org.hamcrest.core.IsEqual;
 import org.junit.Test;
 
 import fr.insaRouen.iti.prog.asiaventure.Monde;
+import fr.insaRouen.iti.prog.asiaventure.elements.Entite;
 
 import static org.hamcrest.core.Is.is;
 import static org.hamcrest.MatcherAssert.assertThat;
+
+class EntiteTest extends Entite {
+    public EntiteTest(String nom, Monde monde) throws NomDEntiteDejaUtiliseDansLeMondeException {
+        super(nom, monde);
+    }
+}
 
 public class TestMonde {
     Monde monde;
@@ -22,5 +29,25 @@ public class TestMonde {
     @Test
     public void testNomMonde() {
         assertThat(monde.getNom(), is("monde"));
+    }
+
+    @Test
+    public void testGetEntite() throws Exception {
+        assertThat(monde.getEntite("entite"), IsEqual.equalTo(null));
+        EntiteTest entite = new EntiteTest("entite", monde);
+        assertThat(monde.getEntite("entite"), IsEqual.equalTo(entite));
+    }
+
+    @Test(expected = NomDEntiteDejaUtiliseDansLeMondeException.class)
+    public void testAjouterEntiteNomDejaUtilise() throws Exception {
+        new EntiteTest("entite", monde);
+        new EntiteTest("entite", monde);
+    }
+
+    @Test(expected = EntiteDejaDansUnAutreMondeException.class)
+    public void testAjouterEntiteDejaDansUnAutreMonde() throws Exception {
+        Monde monde2 = new Monde("monde2");
+        EntiteTest entite = new EntiteTest("entite", monde);
+        monde2.ajouter(entite);
     }
 }
