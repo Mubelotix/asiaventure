740d1fa5e92ae6a69ff98e3ff7a4c7584d54b4cb
Mubelotix <mubelotix@gmail.com>
2024-02-19 17:39:22 +0100

    Fix testvivant

--- srctests/fr/insaRouen/iti/prog/asiaventure/elements/vivants/TestVivant.java
+++ srctests/fr/insaRouen/iti/prog/asiaventure/elements/vivants/TestVivant.java
@@ -2,20 +2,14 @@ package fr.insaRouen.iti.prog.asiaventure.elements.vivants;
 
 import org.junit.Before;
 import org.junit.Test;
-
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.core.Is;
-import static org.hamcrest.core.IsEqual;
-
-import fr.insaRouen.iti.prog.asiaventure.elements.vivants.Vivant;
+import org.hamcrest.core.Is;
+import org.hamcrest.core.IsEqual;
+import org.hamcrest.core.IsNull;
 import fr.insaRouen.iti.prog.asiaventure.Monde;
 import fr.insaRouen.iti.prog.asiaventure.NomDEntiteDejaUtiliseDansLeMondeException;
-import fr.insaRouen.iti.prog.asiaventure.elements.Entite;
 import fr.insaRouen.iti.prog.asiaventure.elements.objets.Objet;
-import fr.insaRouen.iti.prog.asiaventure.elements.objets.ObjetNonDeplacableException;
-import fr.insaRouen.iti.prog.asiaventure.elements.structure.ObjetAbsentDeLaPieceException;
 import fr.insaRouen.iti.prog.asiaventure.elements.structure.Piece;
-import fr.insaRouen.iti.prog.asiaventure.elements.structure.VivantAbsentDeLaPieceException;
 
 class ObjetTest extends Objet {
     public ObjetTest(String nom, Monde monde) throws NomDEntiteDejaUtiliseDansLeMondeException {
@@ -48,35 +42,29 @@ public class TestVivant{
         this.objet = new ObjetTest("objet", this.monde);
         this.piece = new Piece("piece", this.monde);
         this.vivant = new VivantTest("vivant",this.monde, 15, 14, this.piece, this.objet);
-        
-
     }
 
     @Test
     public void testEgalite(){
-        assertThat(this.vivant.getNom(),IsEqual.equalTo("humain"));
+        assertThat(this.vivant.getNom(),IsEqual.equalTo("vivant"));
         assertThat(this.vivant.getPointsForce(), IsEqual.equalTo(14));
         assertThat(this.vivant.getPointsVie(), IsEqual.equalTo(15));
-        assertThat(this.vivant.getObjets().getNom(), IsEqual.equalTo("objet"));
+        assertThat(this.vivant.getObjets()[0].getNom(), IsEqual.equalTo("objet"));
         assertThat(this.vivant.getPiece().getNom(), IsEqual.equalTo("piece"));
     }
 
     @Test
-    public void testObjets(){
+    public void testObjets() throws Exception {
         assertThat(this.vivant.contientObjet(this.objet), Is.is(true));
-        ObjetTest obj = new ObjetTest("tarte");
+        ObjetTest obj = new ObjetTest("tarte", this.monde);
         assertThat(this.vivant.contientObjet(obj), Is.is(false));
     }
 
     @Test
     public void testSortirEntrer(){
         this.vivant.sortir();
-        assertThat(this.vivant.getPiece(),Is.is(null));
+        assertThat(this.vivant.getPiece(),IsNull.nullValue());
         this.vivant.entrer(this.piece);
         assertThat(this.vivant.getPiece(),IsEqual.equalTo(this.piece));
     }
-
-
-
-
 }
