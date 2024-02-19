33e9f02c43182669652b09f3a94d54a20014cd3d
mgevia <mgevia@insa-rouen.fr>
2024-02-19 15:31:02 +0100

    ca marche tjr pas pb de compilation

--- compile-and-test.sh
+++ compile-and-test.sh
@@ -2,7 +2,7 @@
 
 rm -rf classes classestests
 
-javac -classpath ./classes ./classestests /usr/share/java/hamcrest-library.jar /usr/share/java/junit4.jar -d ./classes/ ./classestest -sourcepath ./src/ ./src/fr/insaRouen/iti/prog/asiaventure/Main.java ./srctests/ ./srctests/MondeTests.java
+javac -classpath ./classes:./classestests:/usr/share/java/hamcrest-library.jar:/usr/share/java/junit4.jar -d ./classestest -sourcepath ./srctests/ ./srctests/fr/insaRouen/iti/prog/asiaventure/TestEntite.java
 cd classestests
-java -ea org.junit.runner.JUnitCore MondeTests
+java -ea org.junit.runner.JUnitCore:usr.share.java.hamcrest-library.jar:usr.share.java.junit4.jar: fr.insaRouen.iti.prog.asiaventure.TestEntite
 cd ..
