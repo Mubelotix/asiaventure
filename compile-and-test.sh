ef241840c5a9107a7e6c6525b827db2be8d0d995
Mubelotix <mubelotix@gmail.com>
2024-03-11 14:54:51 +0100

    Fix compilation

--- compile-and-test.sh
+++ compile-and-test.sh
@@ -8,5 +8,5 @@ rm -rf classestest
 # java -ea -classpath ./classes:./classestest:/usr/share/java/hamcrest-core.jar:/usr/share/java/junit4.jar org.junit.runner.JUnitCore fr.insaRouen.iti.prog.asiaventure.AllTests
 # cd ..
 
-javac -classpath ./classes:./classestest:/usr/share/java/* -sourcepath ./srctests -d ./classestest ./srctests/fr/insaRouen/iti/prog/asiaventure/AllTests.java
+javac -classpath ./classes:./classestest:/usr/share/java/* -sourcepath ./src/:./srctests -d ./classestest ./srctests/fr/insaRouen/iti/prog/asiaventure/AllTests.java
 java -classpath ./classes:./classestest:/usr/share/java/* org.junit.runner.JUnitCore fr.insaRouen.iti.prog.asiaventure.AllTests
