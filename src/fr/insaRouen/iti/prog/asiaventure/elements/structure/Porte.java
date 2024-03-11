82f0575177cd8c41f5de6d95042189858a1bfbda
Mubelotix <mubelotix@gmail.com>
2024-03-11 15:54:22 +0100

    Add Porte

---
+++ src/fr/insaRouen/iti/prog/asiaventure/elements/structure/Porte.java
@@ -0,0 +1,50 @@
+package fr.insaRouen.iti.prog.asiaventure.elements.structure;
+
+import fr.insaRouen.iti.prog.asiaventure.Monde;
+import fr.insaRouen.iti.prog.asiaventure.NomDEntiteDejaUtiliseDansLeMondeException;
+import fr.insaRouen.iti.prog.asiaventure.elements.Activable;
+import fr.insaRouen.iti.prog.asiaventure.elements.ActivationException;
+import fr.insaRouen.iti.prog.asiaventure.elements.ActivationImpossibleAvecObjetException;
+import fr.insaRouen.iti.prog.asiaventure.elements.ActivationImpossibleException;
+import fr.insaRouen.iti.prog.asiaventure.elements.Etat;
+import fr.insaRouen.iti.prog.asiaventure.elements.objets.Objet;
+
+public class Porte extends ElementStructurel implements Activable {
+    private Etat etat;
+    private Object serrure;
+    private Piece pieceA;
+    private Piece pieceB;
+
+    public Porte(String nom, Monde monde, Piece pieceA, Piece pieceB) throws NomDEntiteDejaUtiliseDansLeMondeException {
+        super(nom, monde);
+        this.pieceA = pieceA;
+        this.pieceA = pieceB;
+    }
+
+    public Porte(String nom, Monde monde, Object serrure, Piece pieceA, Piece pieceB) throws NomDEntiteDejaUtiliseDansLeMondeException {
+        super(nom, monde);
+        this.pieceA = pieceA;
+        this.pieceA = pieceB;
+        this.serrure = serrure;
+    }
+
+    public boolean activableAvec(Objet obj) {
+        return false;
+    }
+
+    public void activer() throws ActivationImpossibleException {
+        // TODO    
+    }
+
+    public void activerAvec(Objet obj) throws ActivationImpossibleException, ActivationImpossibleAvecObjetException {
+        throw new ActivationImpossibleAvecObjetException();
+    }
+
+    public Etat getEtat() {
+        return this.etat; // TODO: Clone
+    }
+
+    public Object getSerrure() {
+        return null; // TODO: serrure
+    }
+}
