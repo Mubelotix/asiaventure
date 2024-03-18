a02cb446b0cc0220eca920b530d4dab3fccc3836
Mubelotix <mubelotix@gmail.com>
2024-03-18 17:16:59 +0100

    Add test

--- srctests/fr/insaRouen/iti/prog/asiaventure/elements/structure/TestPorte.java
+++ srctests/fr/insaRouen/iti/prog/asiaventure/elements/structure/TestPorte.java
@@ -2,6 +2,10 @@ package fr.insaRouen.iti.prog.asiaventure.elements.structure;
 
 import org.junit.Test;
 import fr.insaRouen.iti.prog.asiaventure.Monde;
+import fr.insaRouen.iti.prog.asiaventure.elements.Etat;
+import fr.insaRouen.iti.prog.asiaventure.elements.objets.Clef;
+import fr.insaRouen.iti.prog.asiaventure.elements.serrurerie.Serrure;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.IsEqual.equalTo;
 
@@ -15,5 +19,29 @@ public class TestPorte {
 
         assertThat(porte.getPieceAutreCote(pA), equalTo(pB));
         assertThat(porte.getPieceAutreCote(pB), equalTo(pA));
+    }
+
+    @Test
+    public void testOuvrirPorte() throws Exception {
+        Monde monde = new Monde("monde");
+        Piece pA = new Piece("pA", monde);
+        Piece pB = new Piece("pB", monde);
+        Serrure serrure = new Serrure("serrure", monde);
+        Clef clef = serrure.creerClef();
+        Porte porte_verrouillee = new Porte("porte", monde, serrure, pA, pB);
+
+        assertThat(porte_verrouillee.getEtat(), equalTo(Etat.VERROUILLE));
+        porte_verrouillee.activer();
+        assertThat(porte_verrouillee.getEtat(), equalTo(Etat.VERROUILLE));
+        porte_verrouillee.activerAvec(clef);
+        assertThat(porte_verrouillee.getEtat(), equalTo(Etat.OUVERT));
+        porte_verrouillee.activer();
+        assertThat(porte_verrouillee.getEtat(), equalTo(Etat.FERME));
+        porte_verrouillee.activer();
+        assertThat(porte_verrouillee.getEtat(), equalTo(Etat.OUVERT));
+        porte_verrouillee.activerAvec(clef);
+        assertThat(porte_verrouillee.getEtat(), equalTo(Etat.VERROUILLE));
+        porte_verrouillee.activerAvec(clef);
+        assertThat(porte_verrouillee.getEtat(), equalTo(Etat.FERME));
     }
 }
