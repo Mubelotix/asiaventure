6e549baadfece612105d48b6a2de35920c16404a
Mubelotix <mubelotix@gmail.com>
2024-03-18 16:25:25 +0100

    Improve Porte.activer

--- src/fr/insaRouen/iti/prog/asiaventure/elements/structure/Porte.java
+++ src/fr/insaRouen/iti/prog/asiaventure/elements/structure/Porte.java
@@ -3,29 +3,32 @@ package fr.insaRouen.iti.prog.asiaventure.elements.structure;
 import fr.insaRouen.iti.prog.asiaventure.Monde;
 import fr.insaRouen.iti.prog.asiaventure.NomDEntiteDejaUtiliseDansLeMondeException;
 import fr.insaRouen.iti.prog.asiaventure.elements.Activable;
+import fr.insaRouen.iti.prog.asiaventure.elements.ActivationException;
 import fr.insaRouen.iti.prog.asiaventure.elements.ActivationImpossibleAvecObjetException;
 import fr.insaRouen.iti.prog.asiaventure.elements.ActivationImpossibleException;
 import fr.insaRouen.iti.prog.asiaventure.elements.Etat;
+import fr.insaRouen.iti.prog.asiaventure.elements.objets.Clef;
 import fr.insaRouen.iti.prog.asiaventure.elements.objets.Objet;
+import fr.insaRouen.iti.prog.asiaventure.elements.objets.PiedDeBiche;
+import fr.insaRouen.iti.prog.asiaventure.elements.serrurerie.Serrure;
 
 public class Porte extends ElementStructurel implements Activable {
-    private Etat etat;
-    private Object serrure;
+    private boolean ouvert = false;
+    private boolean casse = false;
+    private Serrure serrure;
     private Piece pieceA;
     private Piece pieceB;
 
     public Porte(String nom, Monde monde, Piece pieceA, Piece pieceB) throws NomDEntiteDejaUtiliseDansLeMondeException {
         super(nom, monde);
-        this.etat = Etat.OUVERT;
         this.pieceA = pieceA;
         this.pieceB = pieceB;
         this.pieceA.addPorte(this);
         this.pieceB.addPorte(this);
     }
 
-    public Porte(String nom, Monde monde, Object serrure, Piece pieceA, Piece pieceB) throws NomDEntiteDejaUtiliseDansLeMondeException {
+    public Porte(String nom, Monde monde, Serrure serrure, Piece pieceA, Piece pieceB) throws NomDEntiteDejaUtiliseDansLeMondeException {
         super(nom, monde);
-        this.etat = Etat.OUVERT;
         this.pieceA = pieceA;
         this.pieceB = pieceB;
         this.serrure = serrure;
@@ -38,24 +41,62 @@ public class Porte extends ElementStructurel implements Activable {
     }
 
     public void activer() throws ActivationImpossibleException {
-        switch(this.etat) {
+        switch(this.getEtat()) {
             case OUVERT:
-                this.etat = Etat.FERME;
+                this.ouvert = false;
                 break;
             case FERME:
-                this.etat = Etat.OUVERT;
+                this.ouvert = true;
+                break;
+            case VERROUILLE:
+                break;
+            case CASSE:
                 break;
             default:
-                throw new ActivationImpossibleException("La porte est fermée.");
+                throw new ActivationImpossibleException("La porte est dans un état imprévu.");
         }
     }
 
-    public void activerAvec(Objet obj) throws ActivationImpossibleException, ActivationImpossibleAvecObjetException {
-        throw new ActivationImpossibleAvecObjetException("La porte ne peut pas être activée avec un objet.");
+    public void activerAvec(Objet obj) throws ActivationException, ActivationImpossibleException {
+        switch(this.getEtat()) {
+            case OUVERT:
+                if (obj instanceof Clef && obj != null && this.serrure != null) {
+                    this.activer();
+                    this.serrure.activerAvec(obj);
+                }
+                break;
+            case FERME:
+                if (obj instanceof Clef && obj != null && this.serrure != null) {
+                    this.serrure.activerAvec(obj);
+                } else if (obj instanceof PiedDeBiche && obj != null) {
+                    this.casse = true;
+                }
+                break;
+            case VERROUILLE:
+                if (obj instanceof Clef && obj != null && this.serrure != null) {
+                    this.serrure.activerAvec(obj);
+                    this.activer();
+                } else if (obj instanceof PiedDeBiche && obj != null) {
+                    this.casse = true;
+                }
+                break;
+            case CASSE:
+                break;
+            default:
+                throw new ActivationImpossibleException("La porte est dans un état imprévu.");
+        }
     }
 
     public Etat getEtat() {
-        return this.etat;
+        if (this.casse) {
+            return Etat.CASSE;
+        } else if (this.ouvert) {
+            return Etat.OUVERT;
+        } else if (this.serrure != null && this.serrure.getEtat() == Etat.VERROUILLE) {
+            return Etat.VERROUILLE;
+        } else {
+            return Etat.FERME;
+        }
     }
 
     public Object getSerrure() {
