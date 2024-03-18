ddee0058a3a37bdbb1121f295926f70472ab0f90
mgevia <mgevia@insa-rouen.fr>
2024-03-18 16:26:31 +0100

    Classe serrure

--- src/fr/insaRouen/iti/prog/asiaventure/elements/serrurerie/Serrure.java
+++ src/fr/insaRouen/iti/prog/asiaventure/elements/serrurerie/Serrure.java
@@ -6,12 +6,13 @@ import fr.insaRouen.iti.prog.asiaventure.elements.ActivationImpossibleAvecObjetE
 import fr.insaRouen.iti.prog.asiaventure.elements.ActivationException;
 import fr.insaRouen.iti.prog.asiaventure.elements.objets.Objet;
 import fr.insaRouen.iti.prog.asiaventure.elements.Activable;
-import fr.insaRouen.iti.prog.asiaventure.elements.objets.PiedDeBiche;
 import fr.insaRouen.iti.prog.asiaventure.elements.objets.Clef;
+import fr.insaRouen.iti.prog.asiaventure.elements.Etat;
 
 public class Serrure extends Objet implements Activable{
     private Etat etat = Etat.VERROUILLE;
-    private static int numero = 0; 
+    private static int numero = 0;
+    private static Clef clef;
 
     public Serrure(String nom, Monde monde) throws NomDEntiteDejaUtiliseDansLeMondeException{
         super(nom, monde);
@@ -20,22 +21,34 @@ public class Serrure extends Objet implements Activable{
     public Serrure (Monde monde) throws NomDEntiteDejaUtiliseDansLeMondeException{
         super(String.format("%d", ++numero), monde);
     }
+/*
+    public final Clef creerClef(){
+        if(this.clef == null){
+            this.clef = new Clef("clef", );
+        }
+    }*/
+
+    public void activerAvec(Objet objet) throws ActivationImpossibleAvecObjetException{
+        if(! activableAvec(objet)){
+            throw new ActivationImpossibleAvecObjetException(String.format("Activation impossible entre l'objet %s et la serrure %s", objet.getNom(), this.getNom()));
+        }
+        if(this.getEtat().equals(Etat.VERROUILLE)){
+            this.etat = Etat.DEVEROUILLE;
+        }else{
+            this.etat = Etat.VEROUILLE;
+        }
+    }
 
     public boolean activableAvec(Objet objet){
-        if(objet instanceof PiedDeBiche){
-
-        }
-        return true;
+        return objet instanceof Clef;
     }
 
-    public void activableAvec(Objet objet){
-    }
-
-    public void activer(){
+    public void activer() throws ActivationImpossibleException{
+    
     }
 
     public boolean estDeplacable(){
-        return true;
+        return false;
     }
 
     public Etat getEtat(){
