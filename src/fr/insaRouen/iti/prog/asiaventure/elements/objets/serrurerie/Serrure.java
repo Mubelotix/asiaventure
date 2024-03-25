8d60328b68bd6dd2b04c048df396daea734cc292
mgevia <mgevia@insa-rouen.fr>
2024-03-25 15:40:59 +0100

    ajout de la doc dans serrure

--- src/fr/insaRouen/iti/prog/asiaventure/elements/objets/serrurerie/Serrure.java
+++ src/fr/insaRouen/iti/prog/asiaventure/elements/objets/serrurerie/Serrure.java
@@ -18,11 +18,18 @@ public class Serrure extends Objet implements Activable{
 
     /**
      * Constructeur de Serrure 
+     * @param Monde le monde de la serrure
+     * @param String le nom de la serrure
      */
     public Serrure(String nom, Monde monde) throws NomDEntiteDejaUtiliseDansLeMondeException{
         super(nom, monde);
         this.clef = Clef.creer(this.monde);
     }
+
+    /**
+     * Constructeur de Serrure avec un nom automatique
+     * @param Monde le monde de la serrure
+     */
 
     public Serrure (Monde monde) throws NomDEntiteDejaUtiliseDansLeMondeException{
         super(String.format("serrure_%d", numero), monde);
@@ -30,7 +37,10 @@ public class Serrure extends Objet implements Activable{
         ++numero;
     }
 
-
+    /**
+     * Obtenir la clef d'une serrure une unique fois
+     * @return la clef nouvellement créée 
+     */
     public final Clef creerClef() {
         if(this.utilisee){
             return null;
@@ -39,12 +49,22 @@ public class Serrure extends Objet implements Activable{
         return this.clef;
     }
 
+    /**
+     * Activer une serrure avec un objet
+     * @param Objet l'objet permettant peut être de déverouiller/vérouiller la serrure
+     */
     public void activerAvec(Objet objet) throws ActivationImpossibleAvecObjetException {
         if(! activableAvec(objet)){
             throw new ActivationImpossibleAvecObjetException(String.format("Activation impossible entre l'objet %s (ca aurait du etre %s )et la serrure %s", objet.getNom(), this.clef.getNom(),this.getNom()));
         }
         this.activer();
     }
+
+    /**
+     * Vérifier si l'objet permet de vérouiller/déverrouiller la serrure
+     * @param Objet l'objet permettant peut être de déverouiller/vérouiller la serrure
+     * @return la vie
+     */
 
     public boolean activableAvec(Objet objet){
         return (objet instanceof Clef) && (this.clef != null) && this.clef.getNom().equals(objet.getNom());
