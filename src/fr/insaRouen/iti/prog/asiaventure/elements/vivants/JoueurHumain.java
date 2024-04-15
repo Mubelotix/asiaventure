dd5221a4d242aebb5e1ac5fd7bc35c1968294296
mgevia <mgevia@insa-rouen.fr>
2024-04-15 16:17:25 +0200

    modif de executer introspection

--- src/fr/insaRouen/iti/prog/asiaventure/elements/vivants/JoueurHumain.java
+++ src/fr/insaRouen/iti/prog/asiaventure/elements/vivants/JoueurHumain.java
@@ -1,6 +1,9 @@
 package fr.insaRouen.iti.prog.asiaventure.elements.vivants;
 
 import java.util.Scanner;
+import java.lang.reflect.Method;
+import java.util.List;
+import java.util.ArrayList;
 import fr.insaRouen.iti.prog.asiaventure.Monde;
 import fr.insaRouen.iti.prog.asiaventure.NomDEntiteDejaUtiliseDansLeMondeException;
 import fr.insaRouen.iti.prog.asiaventure.elements.structure.PorteFermeException;
@@ -25,25 +28,31 @@ public class JoueurHumain extends Vivant{
     public void executer() throws CommandeImpossiblePourLeVivantException, Throwable{
         Scanner s = new Scanner(this.ordre);
         try{
-            switch(s.next()){
-                case "Prendre":
-                    commandePrendre(s.next());
-                case "Poser":
-                    commandePoser(s.next());
-                case "Franchir":
-                    commandeOuvrirPorte(s.next());
-                case "OuvrirPorte":
-                    String nomPorte = s.next();
-                    if(s.hasNext()){
-                        String nomObjet = s.next();
-                        commandeOuvrirPorte(nomPorte, nomObjet);
-                    }else{
-                        commandeOuvrirPorte(nomPorte);
-                    }
-                default:
-                    throw new CommandeImpossiblePourLeVivantException("cette commande n'existe pas");
+            //switch(s.next()){
+            //    case "Prendre":
+            //        commandePrendre(s.next());
+            //    case "Poser":
+            //        commandePoser(s.next());
+            //    case "Franchir":
+            //        commandeOuvrirPorte(s.next());
+            //    case "OuvrirPorte":
+            //        String nomPorte = s.next();
+            //        if(s.hasNext()){
+            //            String nomObjet = s.next();
+            //            commandeOuvrirPorte(nomPorte, nomObjet);
+            //        }else{
+            //            commandeOuvrirPorte(nomPorte);
+            //        }
+            //    default:
+            //        throw new CommandeImpossiblePourLeVivantException("cette commande n'existe pas");
+            //}
+            Method methode = this.getClass().getDeclaredMethod(String.format("commande%s", s.next()));
+            List<String> arg = new ArrayList<String>();
+            while(s.hasNext()){
+                arg.add(s.next());
             }
-        }catch(CommandeImpossiblePourLeVivantException e){
+            methode.invoke(this, arg);
+
         }catch(Throwable e1){
             System.out.println(String.format("Cause exception : %s", e1.getCause()));
         }
