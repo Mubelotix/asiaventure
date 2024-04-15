5613e06abe75e6b88827daec14b8fe55f787ba3a
Mubelotix <mubelotix@gmail.com>
2024-04-15 14:59:41 +0200

    Add ConditionDeFinConjonctionConditionDeFin

---
+++ src/fr/insaRouen/iti/prog/asiaventure/ConditionDeFinConjonctionConditionDeFin.java
@@ -0,0 +1,28 @@
+package fr.insaRouen.iti.prog.asiaventure;
+
+public class ConditionDeFinConjonctionConditionDeFin extends ConditionDeFin {
+    ConditionDeFin[] cfs;
+
+    public ConditionDeFinConjonctionConditionDeFin(EtatDuJeu etatDuJeu, ConditionDeFin... cfs) {
+        super(etatDuJeu);
+        this.cfs = cfs;
+    }
+
+    @Override
+    public EtatDuJeu verifierCondition() {
+        boolean verifie = true;
+        for (ConditionDeFin cf : this.cfs) {
+            EtatDuJeu etat = cf.verifierCondition();
+            if (etat == EtatDuJeu.ECHEC) {
+                return EtatDuJeu.ECHEC;
+            } else if (etat == EtatDuJeu.ENCOURS) {
+                verifie = false;
+            }
+        }
+        if (verifie) {
+            return this.getEtatConditionVerifiee();
+        } else {
+            return EtatDuJeu.ENCOURS;
+        }
+    }
+}
