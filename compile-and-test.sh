3ded8c47382fbd74d63035abbf9f54b8f3e36a05
Mubelotix <mubelotix@gmail.com>
2024-04-22 14:46:07 +0200

    Fix test build

--- compile-and-test.sh
+++ compile-and-test.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 rm -rf classestest
+rm -rf classes
 
 # javac -classpath ./classes:./classestest:/usr/share/java/hamcrest-core.jar:/usr/share/java/junit4.jar -d ./classestest -sourcepath ./srctests/ ./srctests/fr/insaRouen/iti/prog/asiaventure/AllTests.java
 # 
@@ -8,5 +9,6 @@ rm -rf classestest
 # java -ea -classpath ./classes:./classestest:/usr/share/java/hamcrest-core.jar:/usr/share/java/junit4.jar org.junit.runner.JUnitCore fr.insaRouen.iti.prog.asiaventure.AllTests
 # cd ..
 
-javac -classpath ./classes:./classestest:/usr/share/java/* -sourcepath ./src/:./srctests -d ./classestest ./srctests/fr/insaRouen/iti/prog/asiaventure/AllTests.java
+javac -classpath ./classes -sourcepath ./src/ -d ./classes/ $(find ./src -name "*.java")
+javac -classpath ./classes:./classestest:/usr/share/java/* -sourcepath ./srctests -d ./classestest ./srctests/fr/insaRouen/iti/prog/asiaventure/AllTests.java
 java -classpath ./classes:./classestest:/usr/share/java/* org.junit.runner.JUnitCore fr.insaRouen.iti.prog.asiaventure.AllTests
